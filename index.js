9a6a79d9ccfa59ace844849c69c594fb62b7d1bf
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 11:16:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ console.log('server is starting')
 var express = require ('express')
 var app = express()
 
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 
-
+/*
 var server = app.listen(5000)
 
 function listening {
   console.log('listening...')
 }
+*/
 
-//  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
