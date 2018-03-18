8afa97e5542a6fedbbb4b4a905604d3c22b65f50
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 15:45:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var header = "../partials/header2.ejs"
-var nav = "../partials/nav4.ejs"
-var footer = "../partials/footer1.ejs"
+var header = "../partials/header.ejs"
+var nav = "../partials/nav.ejs"
+var footer = "../partials/footer.ejs"
 
 var indexData = {
-  title:'page two',
-  content:'bla2 bla2...',
+  title:'index page',
+  content:'index... index... index...',
   testfile:'testfile.ejs', 
   nav: nav,
   header: header,
@@ -20,12 +20,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
- .get('/', (req, res) => res.render('pages/index3',indexData))
-    .get('/pag2', (req, res) => res.render('pages/pag2'))
-    .get('/pag3', (req, res) => res.render('pages/pag3'))
-    .get('/pag4', (req, res) => res.render('pages/pag4'))
-    .get('/pag5', (req, res) => res.render('pages/pag5'))
-.post('bla', (req, res) => res.render('pages/post'))
+ .get('/', (req, res) => res.render('pages/index',indexData))
 
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
