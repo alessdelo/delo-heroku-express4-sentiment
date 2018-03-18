4111d0dad38245eca4db344ff14fe0eacacf159f
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-18 11:54:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-
-// console.log('server is starting')
-var express = require ('express')
-var app = express()
-
+const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
 
-/*
-var server = app.listen(5000)
-
-function listening {
-  console.log('listening...')
-}
-*/
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
